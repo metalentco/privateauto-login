b7fb0fcfa57b9db41b0a189e375ecb63be1c01d9
metalentco <metalentco777@gmail.com>
2023-05-22 22:52:44 -0400

    fix:fontstyle works now

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
 import "../styles/globals.css";
+import { fontRegularStyles, fontBoldStyles } from "../libs/fontStyles";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <style jsx global>
+        {`
+          ${fontRegularStyles}
+          ${fontBoldStyles}
+        `}
+      </style>
+      <Component {...pageProps} />
+    </>
+  );
 }
